d7bc66f45fde600a97224228e4df92e607244679
Tore Birkeland <tore@norbits.no>
2016-03-24 19:43:48 +0100

    Formatting phone number when editing in UserAdmin

--- src/Controllers/UserAdminController.cs
+++ src/Controllers/UserAdminController.cs
@@ -1,7 +1,9 @@
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
 using TandemBooking.Models;
+using TandemBooking.Services;
 
 namespace TandemBooking.Controllers
 {
@@ -9,10 +11,12 @@ namespace TandemBooking.Controllers
     public class UserAdminController : Controller
     {
         private readonly TandemBookingContext _context;
+        private readonly NexmoService _nexmo;
 
-        public UserAdminController(TandemBookingContext context)
+        public UserAdminController(TandemBookingContext context, NexmoService nexmo)
         {
             _context = context;
+            _nexmo = nexmo;
         }
 
         public ActionResult Index()
@@ -32,13 +36,14 @@ namespace TandemBooking.Controllers
         }
 
         [HttpPost]
-        public ActionResult EditUser(string id, ApplicationUser input)
+        public async Task<ActionResult> EditUser(string id, ApplicationUser input)
         {
             if (ModelState.IsValid)
             {
                 var user = _context.Users.First(u => u.Id == id);
                 user.Name = input.Name;
                 user.Email = input.Email;
+                input.PhoneNumber = await _nexmo.FormatPhoneNumber(input.PhoneNumber);
                 user.PhoneNumber = input.PhoneNumber;
                 user.IsPilot = input.IsPilot;
                 user.IsAdmin = input.IsAdmin;
