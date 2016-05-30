507cb4f8447af93cd2daddace597aa6665c3b28d
Tore Birkeland <tore@norbits.no>
2016-05-30 09:44:35 +0200

    Added availability overview

---
+++ src/Controllers/AvailabilityOverviewController.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+using TandemBooking.Models;
+using Microsoft.Data.Entity;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using TandemBooking.Services;
+using TandemBooking.ViewModels.AvailabilityOverview;
+using TandemBooking.ViewModels.BookingAdmin;
+
+namespace TandemBooking.Controllers
+{
+    [Authorize]
+    public class AvailabilityOverviewController : Controller
+    {
+        private static string[] _monthNames = new[]
+        {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        };
+
+        private readonly TandemBookingContext _context;
+        
+        public AvailabilityOverviewController(TandemBookingContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index(DateTime? date=null)
+        { 
+            if (!User.IsAdmin() && !User.IsPilot())
+            {
+                return new HttpUnauthorizedResult();
+            }
+
+            if (date == null)
+            {
+                date = DateTime.UtcNow;
+            }
+
+            var startDate = new DateTime(date.Value.Year, date.Value.Month, 1);
+            var endDate = startDate.AddMonths(1);
+
+            var nextMonth = startDate.AddMonths(1);
+            var prevMonth = startDate.AddMonths(-1);
+
+            int startWeekDay = ((int)startDate.DayOfWeek - 1 + 7) % 7;
+            startDate = startDate.AddDays(-startWeekDay);
+
+            int endWeekDay = ((int)endDate.DayOfWeek - 1 + 7) % 7;
+            endDate = endDate.AddDays(6 - endWeekDay);
+
+            var pilots = _context.Users.ToList();
+
+            var availabilities = _context.PilotAvailabilities
+                .Where(a => a.Date >= startDate && a.Date <= endDate)
+                .OrderBy(a => a.Date)
+                .ToList();
+
+            var pilotBookings = _context.BookedPilots
+                .Include(b => b.Booking)
+                .Where(b => !b.Canceled && b.Booking.BookingDate >= startDate && b.Booking.BookingDate <= endDate)
+                .ToList();
+
+            var unassignedBookings = _context.Bookings
+                .Where(b => b.AssignedPilot == null && !b.Canceled && b.BookingDate >= startDate && b.BookingDate <= endDate)
+                .ToList();
+
+            var days = new List<AvailabilityOverviewDayViewModel>();
+
+            for (var dayIndex = 0; startDate.AddDays(dayIndex) <= endDate; dayIndex++)
+            {
+                var curDate = startDate.AddDays(dayIndex);
+                var availableToday = availabilities.Where(a => a.Date.Date == curDate).ToList();
+                var unassignedToday = unassignedBookings.Where(b => b.BookingDate.Date == curDate).ToList();
+                var bookingsToday = pilotBookings.Where(a => a.Booking.BookingDate.Date == curDate).ToList();
+                days.Add(new AvailabilityOverviewDayViewModel()
+                {
+                    Date = curDate,
+                    Availabilities = availableToday,
+                    PilotBookings = bookingsToday,
+                    UnassignedBookings = unassignedBookings,
+                    FreePilots = availableToday.Where(a => bookingsToday.All(b => b.Pilot != a.Pilot)).ToList(),
+                    InPast = curDate < DateTime.Now.Date,
+                });                
+            }
+
+            return View(new AvailabilityOverviewViewModel()
+            {
+                Next = nextMonth,
+                Prev = prevMonth,
+                MonthName = $"{_monthNames[date.Value.Month - 1]} {date.Value.Year}",
+                StartDate = startDate,
+                EndDate = endDate,
+                Days = days,
+            });
+        }
+    }
+}
---
+++ src/ViewModels/AvailabilityOverview/AvailabilityOverviewViewModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using TandemBooking.Models;
+
+namespace TandemBooking.ViewModels.AvailabilityOverview
+{
+    public class AvailabilityOverviewViewModel
+    {
+        public DateTime Next { get; set; }
+        public DateTime Prev { get; set; }
+        public string MonthName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<AvailabilityOverviewDayViewModel> Days { get; set; }
+    }
+
+    public class AvailabilityOverviewDayViewModel
+    {
+        public DateTime Date { get; set; }
+        public List<PilotAvailability> Availabilities { get; set; }
+        public List<BookedPilot> PilotBookings { get; set; }
+        public List<PilotAvailability> FreePilots { get; set; }
+        public bool InPast { get; set; }
+        public List<Models.Booking> UnassignedBookings { get; set; }
+    }
+}
