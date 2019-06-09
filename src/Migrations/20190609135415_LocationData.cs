e9db0390cff11254cfade389e155b287f8c0efa0
Tore Birkeland <tore@norbits.no>
2019-06-09 15:35:27 +0200

    Added active field to location

---
+++ src/Migrations/20190609135415_LocationData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TandemBooking.Models;
+
+namespace tandembooking.Migrations
+{
+    public partial class LocationData: Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Location", new string[]
+            {
+                nameof(Location.Id),
+                nameof(Location.Name),
+                nameof(Location.Active)
+            }, new object[,]
+            {
+                {
+                    Guid.NewGuid(),
+                    "Voss",
+                    true
+                },
+                {
+                    Guid.NewGuid(),
+                    "Myrkdalen",
+                    true
+                },
+                {
+                    Guid.NewGuid(),
+                    "Aurland",
+                    true
+                }
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
